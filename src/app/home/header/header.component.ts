9a1fc1c153b549ea17ee99619579606da08ab68d
Jameson Taylor <jameson@ionic.io>
2023-02-02 15:55:21 -0500

    Add comments to explain ion-back-button limitations; Add comments to add directions for comparing use-case to "normal";

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -6,6 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  // If consumer does not provide defaultHref, ion-back-button will NEVER appear, even when expected
   @Input() defaultHref?: string;
   @Input() title?: string;
   @Input() color?: string;
